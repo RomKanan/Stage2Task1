067a4e0d64978e5e24232b0cbc9c23268b966464
Roman Kananovich <roman.kananovich@gmail.com>
2019-06-02 22:02:53 +0300

    initial

---
+++ Task1/Task1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task1
+//
+//  Created by Roma on 6/2/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Task1/Task1/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Task1
+//
+//  Created by Roma on 6/2/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
