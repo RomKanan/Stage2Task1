5a2b176464b736e8a0146d7971364845409257fc
Roman Kananovich <roman.kananovich@gmail.com>
2019-06-02 22:08:31 +0300

    custom view added

---
+++ Task1/Task1/CustomView.h
@@ -0,0 +1,17 @@
+//
+//  CustomView.h
+//  Task1
+//
+//  Created by Roma on 6/2/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
